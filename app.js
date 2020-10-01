c4c626a782eefb235898a88ee69ef2b9ed5f37fe
NeyNey <drfrankenstein123@gmail.com>
2020-09-30 22:11:22 -0500

    fixed a bug

--- app.js
+++ app.js
@@ -17,128 +17,125 @@ const membersOfTeam = [];
 // and to create objects for each team member (using the correct classes as blueprints!)
 function createManager() {
     inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "enter name please",
-            validate: answer => {
-                if(answer !== "") {
-                    return true;
+        .prompt([{
+                type: "input",
+                name: "name",
+                message: "enter name please",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "entry must be more than one character."
                 }
-                return "entry must be more than one character."
-            }
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "enter id please",
-            validate: answer => {
-                const pass = answer.match(/^[1-9]\d*$/);
-                if(pass) {
-                    return true;
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "enter id please",
+                validate: answer => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+                    if (pass) {
+                        return true;
+                    }
+                    return "no sybols or letters allowed, numerical passwords only"
                 }
-                return "no sybols or letters allowed, numerical passwords only"
-            }
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "enter your email addrress.",
-            validate: answer => {
-                const pass = answer.match(/\S+@\S+\.\S+/);
-                if (pass) {
-                    return true;
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "enter your email addrress.",
+                validate: answer => {
+                    const pass = answer.match(/\S+@\S+\.\S+/);
+                    if (pass) {
+                        return true;
+                    }
+                    return "email not valid."
                 }
-                return "email not valid."
-            }
-        },
-       {
-           type: "input",
-           name: "office",
-           message: "enter your office number please.",
-           validate: answer => {
-               const pass = answer.match(/^[1-9]\d*$/);
-               if(pass) {
-                   return true;
-               }
-               return "no sybols or letters allowed, numerical passwords only"
-       } 
-    },
-    ]).then(function (answers) {
-        const manager = new Manager(answers.name, answers.id, answers.email, answers.office);
-        membersOfTeam.push(manager);
-        addTOTEAM();
+            },
+            {
+                type: "input",
+                name: "office",
+                message: "enter your office number please.",
+                validate: answer => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+                    if (pass) {
+                        return true;
+                    }
+                    return "no sybols or letters allowed, numerical passwords only"
+                }
+            },
+        ]).then(function (answers) {
+            const manager = new Manager(answers.name, answers.id, answers.email, answers.office);
+            membersOfTeam.push(manager);
+            addTOTEAM();
 
-    })
+        })
 }
 
 function createEngineer() {
     inquirer
-    .prompt([
-        {
+        .prompt([{
 
-            type: "input",
-            name: "name",
-            message: "engineer name",
-            
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "engineer id please"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "engineer email address please",
-        },
-        {
-            type: "input",
-            name: "github",
-            message: "enter engineers git hub username.",
+                type: "input",
+                name: "name",
+                message: "engineer name",
 
-        },
-      
-    ]).then(function(answersEng) {
-        const engineer = new Engineer(answersEng.name, answersEng.id, answersEng.github);
-        membersOfTeam.push(engineer);
-        addTOTEAM();
-        
-    })
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "engineer id please"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "engineer email address please",
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "enter engineers git hub username.",
+
+            },
+
+        ]).then(function (answersEng) {
+            const engineer = new Engineer(answersEng.name, answersEng.id, answersEng.email, answersEng.github);
+            membersOfTeam.push(engineer);
+            addTOTEAM();
+
+        })
 }
 
 function createIntern() {
     inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "intern name please",
+        .prompt([{
+                type: "input",
+                name: "name",
+                message: "intern name please",
 
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "intern id please"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "intern email please",
-        },
-        {
-            type: "input",
-            name: "school",
-            message: "school intern attended",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "intern id please"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "intern email please",
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "school intern attended",
 
-        },
+            },
 
-    ]).then(function(answersInt) {
-        const intern = new Intern(answersInt.name, answersInt.id, answersInt.email, answersInt.school);
-        membersOfTeam.push(intern);
-        addTOTEAM();
-    })
+        ]).then(function (answersInt) {
+            const intern = new Intern(answersInt.name, answersInt.id, answersInt.email, answersInt.school);
+            membersOfTeam.push(intern);
+            addTOTEAM();
+        })
 }
 
 
@@ -147,27 +144,25 @@ function createIntern() {
 // generate and return a block of HTML including templated divs for each employee!
 function addTOTEAM() {
     inquirer
-    .prompt([
-      {
-          type: "list",
-          name: "employeeType",
-          message: "what kind of team member will you be adding?",
-          choices: [
-              "Engineer",
-              "intern",
-              "not adding new team member."
-          ]
-      }  
-    ]).then(function (answer){
-        if(answer.employeeType === "Engineer") {
-            createEngineer();
-        }else if(answer.employeeType === "intern") {
-            createIntern();
-        }else {
-            console.log("we created your team!")
-            makeTeam();
-        }
-    })
+        .prompt([{
+            type: "list",
+            name: "employeeType",
+            message: "what kind of team member will you be adding?",
+            choices: [
+                "Engineer",
+                "intern",
+                "not adding new team member."
+            ]
+        }]).then(function (answer) {
+            if (answer.employeeType === "Engineer") {
+                createEngineer();
+            } else if (answer.employeeType === "intern") {
+                createIntern();
+            } else {
+                console.log("we created your team!")
+                makeTeam();
+            }
+        })
 }
 
 // After you have your html, you're now ready to create an HTML file using the HTML
@@ -176,7 +171,7 @@ function addTOTEAM() {
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 function makeTeam() {
-    if(!fs.existsSync(OUTPUT_DIR)) {
+    if (!fs.existsSync(OUTPUT_DIR)) {
         fs.mkdirSync(OUTPUT_DIR)
     }
     fs.writeFileSync(outputPath, render(membersOfTeam), "utf-8");
